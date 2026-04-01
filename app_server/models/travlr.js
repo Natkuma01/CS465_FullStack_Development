cc57a9ddcad489f9187ebefbf4f33147d87f35ad
Natalie Chan <natkuma@outlook.com>
2026-03-31 22:21:03 -0400

    Update Trip schema with required fields, defaults, and matching JSON keys

--- app_server/models/travlr.js
+++ app_server/models/travlr.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
 
 const tripSchema = new mongoose.Schema({
-    code: { type: String, required: true, index: true },
+    code: { type: String, required: true, index: true, unique: true },
     name: { type: String, required: true, index: true },
     length: { type: String, required: true },
     start: { type: Date, required: true },
     resort: { type: String, required: true },
-    perPerson: { type: String, required: true },
-    image: { type: String, required: true },
+    perPerson: { type: Number, required: true },
+    image: { type: String, required: true, default: 'reef1.jpg' },
     description: { type: String, required: true },
 });
 
